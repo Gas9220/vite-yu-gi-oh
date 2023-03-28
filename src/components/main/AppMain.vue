9f9590435c37acba48c26af9f368d570da1b6752
Gaspare <Gas9220@users.noreply.github.com>
2023-03-28 09:30:29 +0200

    Bonus: Aggiunto il caricamento

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -23,11 +23,74 @@ export default {
                 <div class="p-2 bg-black text-white">Found {{store.cards.length}} cards</div>
             </div>
             <CardElement v-for="card in store.cards" v-if="store.cards.length === 39" :img="card.card_images[0].image_url" :text1="card.name" :text2="card.race"></CardElement>
-            <div v-else class="col-12">
-                
+            <div v-else class="align-self-center p-3 text-center">
+                <span class="loader"><span class="loader-inner"></span></span>
             </div>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/_partial/variables' as *;
+.loader {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  border: 4px solid black;
+  top: 50%;
+  animation: loader 2s infinite ease;
+}
+
+.loader-inner {
+  vertical-align: top;
+  display: inline-block;
+  width: 100%;
+  background-color: $primary-color;
+  animation: loader-inner 2s infinite ease-in;
+}
+
+@keyframes loader {
+  0% {
+    transform: rotate(0deg);
+  }
+  
+  25% {
+    transform: rotate(180deg);
+  }
+  
+  50% {
+    transform: rotate(180deg);
+  }
+  
+  75% {
+    transform: rotate(360deg);
+  }
+  
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loader-inner {
+  0% {
+    height: 0%;
+  }
+  
+  25% {
+    height: 0%;
+  }
+  
+  50% {
+    height: 100%;
+  }
+  
+  75% {
+    height: 100%;
+  }
+  
+  100% {
+    height: 0%;
+  }
+}
+</style>
